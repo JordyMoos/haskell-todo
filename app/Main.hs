4fa32e663da929ace21b1a14159c957a95e445c7
Jordy Moos <jordy@pbwebmedia.com>
2019-06-17 17:38:11 +0200

    Playing with json

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,206 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
-    
-import Options.Applicative
+
+import           Data.Aeson
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Yaml as Yaml
+import           GHC.Generics
+import           Options.Applicative     hiding ( infoParser )
+
 
 type ItemIndex = Int
-
+type ItemTitle = String
 type ItemDescription = Maybe String
+type ItemPriority = Maybe String
+type ItemDueBy = Maybe String
 
-data Options = Options FilePath ItemIndex ItemDescription deriving Show
+data Item = Item
+    { title :: ItemTitle
+    , description :: ItemDescription
+    , priority :: ItemPriority
+    , dueBy :: ItemDueBy
+    } deriving (Generic, Show)
+instance ToJSON Item
+instance FromJSON Item
 
+data ItemUpdate = ItemUpdate
+    { titleUpdate :: Maybe ItemTitle
+    , descriptionUpdate :: Maybe ItemDescription
+    , priorityUpdate :: Maybe ItemPriority
+    , dueByUpdate :: Maybe ItemDueBy
+    } deriving Show
+
+
+data ToDoList = ToDoList [Item] deriving (Generic, Show)
+instance ToJSON ToDoList
+instance FromJSON ToDoList
+
+
+data Options = Options FilePath Command deriving Show
+
+data Command
+    = Info
+    | Init
+    | List
+    | Add Item
+    | View ItemIndex
+    | Update ItemIndex ItemUpdate
+    | Remove ItemIndex
+    deriving Show
 
 defaultDataPath :: FilePath
 defaultDataPath = "~/.to-do.yaml"
 
 
-optionsParser :: Parser Options
-optionsParser = Options
-    <$> dataPathParser
-    <*> itemIndexParser
-    <*> updateItemDescriptionParser
+infoParser :: Parser Command
+infoParser = pure Info
 
 
-
-dataPathParser :: Parser FilePath
-dataPathParser = strOption $
-    value defaultDataPath
-    <> long "data-path"
-    <> short 'p'
-    <> metavar "DATAPATH"
-    <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+initParser :: Parser Command
+initParser = pure Init
 
 
-itemIndexParser :: Parser ItemIndex
-itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+listParser :: Parser Command
+listParser = pure List
 
 
-itemDescriptionValueParser  :: Parser String
-itemDescriptionValueParser =
-    strOption (long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description")
+addParser :: Parser Command
+addParser =
+    Add <$> addItemparser
+
+
+addItemparser :: Parser Item
+addItemparser =
+    Item
+        <$> argument str (metavar "TITLE" <> help "title")
+        <*> optional itemDescriptionValueParser
+        <*> optional itemPriorityValueParser
+        <*> optional itemDueByValueParser
+
+
+viewParser :: Parser Command
+viewParser =
+    View <$> itemIndexParser
+
+
+updateParser :: Parser Command
+updateParser =
+    Update
+        <$> itemIndexParser
+        <*> updateItemParser
+
+
+updateItemParser :: Parser ItemUpdate
+updateItemParser =
+    ItemUpdate
+        <$> optional updateItemTitleParser
+        <*> optional updateItemDescriptionParser
+        <*> optional updateItemPriorityParser
+        <*> optional updateItemDueByParser
+
+
+updateItemTitleParser :: Parser ItemTitle
+updateItemTitleParser = itemTitleValueParser
+
 
 updateItemDescriptionParser :: Parser ItemDescription
 updateItemDescriptionParser =
     Just <$> itemDescriptionValueParser
     <|> flag' Nothing (long "clear-desc")
 
+
+updateItemPriorityParser :: Parser ItemPriority
+updateItemPriorityParser =
+    Just <$> itemPriorityValueParser
+    <|> flag' Nothing (long "clear-priority")
+
+
+updateItemDueByParser :: Parser ItemPriority
+updateItemDueByParser =
+    Just <$> itemDueByValueParser
+    <|> flag' Nothing (long "clear-due-by")
+
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+
+
+itemTitleValueParser :: Parser String
+itemTitleValueParser =
+    strOption $ long "title" <> short 't' <> metavar "TITLE" <> help "title"
+
+
+itemDescriptionValueParser :: Parser String
+itemDescriptionValueParser =
+    strOption $ long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description"
+
+
+itemPriorityValueParser :: Parser String
+itemPriorityValueParser =
+    strOption $ long "priority" <> short 'p' <> metavar "PRIORITY" <> help "priority"
+
+
+itemDueByValueParser :: Parser String
+itemDueByValueParser =
+    strOption $ long "due-by" <> short 'b' <> metavar "DUEBY" <> help "due-by date/time"
+
+
+removeParser :: Parser Command
+removeParser =
+    Remove <$> itemIndexParser
+
+
+commandParser :: Parser Command
+commandParser = subparser $ mconcat
+    [ command "info" $ info infoParser (progDesc "Show info")
+    , command "init" $ info initParser (progDesc "Initialize items")
+    , command "list" $ info listParser (progDesc "List all items")
+    , command "add" $ info addParser (progDesc "Add item")
+    , command "view" $ info viewParser (progDesc "View item")
+    , command "update" $ info updateParser (progDesc "Update item")
+    , command "remove" $ info removeParser (progDesc "Remove item")
+    ]
+
+
+optionsParser :: Parser Main.Options
+optionsParser =
+    Options
+        <$> dataPathParser
+        <*> commandParser
+
+
+dataPathParser :: Parser FilePath
+dataPathParser =
+    strOption
+        $  value defaultDataPath
+        <> long "data-path"
+        <> short 'p'
+        <> metavar "DATAPATH"
+        <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+
 main :: IO ()
 main = do
-    options <- execParser (info (optionsParser) (progDesc "To-do list manager"))
-    putStrLn $ "options=" ++ show options
+    BSL.putStrLn $ encode $ ToDoList
+        [ Item "title1" (Just "description 1") (Just "priority1") (Just "dueBy1")
+        , Item "title2" (Just "description 2") (Just "priority2") (Just "dueBy2")
+        ]
+    -- Options dataPath command <- execParser $ info optionsParser (progDesc "To-do list manager")
+    -- run dataPath command
 
+
+run :: FilePath -> Command -> IO ()
+run dataPath Info = putStrLn "Info"
+run dataPath Init = putStrLn "Init"
+run dataPath List = putStrLn "List"
+run dataPath (Add item) =
+    putStrLn $ "Add: item=" ++ show item
+run dataPath (View idx) = 
+    putStrLn $ "View: idx=" ++ show idx
+run dataPath (Update idx itemUpdate) =
+    putStrLn $ "Update: idx=" ++ show idx ++ " itemUpdate=" ++ show itemUpdate
+run dataPath (Remove idx) = 
+    putStrLn $ "Remove: idx=" ++ show idx
